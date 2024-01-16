7073ec6bc22c1aee99a390c98b098a34adba47bc
Leonardo Pereira Sampaio <profleonardo95@gmail.com>
2024-01-16 14:22:30 -0300

    resultados dos exercicios "qual é o numero" e "fiboncci"

---
+++ src/Fibonacci.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fibonacci {
+    public static void main(String[] args) throws Exception {
+        int x = 4;        
+        List<Integer> sequenciaFibonacci = new ArrayList<>();
+        
+        if(x == 0) System.out.println("x é igual a 0");
+        if(x == 1) System.out.println("0");
+
+        if(x != 1 && x != 0){
+            sequenciaFibonacci.add(0);
+            sequenciaFibonacci.add(1);
+             for(int i = 0; i < x; i++){
+                int ultimo = (sequenciaFibonacci.get(sequenciaFibonacci.size()-1));
+                int penultimo = (sequenciaFibonacci.get(sequenciaFibonacci.size()-2));
+                sequenciaFibonacci.add(ultimo + penultimo);
+                System.out.print(sequenciaFibonacci.get(i) + " ");
+            }
+        }       
+    }
+}
+
+//Dado um número inteiro x, imprima uma sequência de Fibonacci de tamanho x.
+
+// Exemplo de saída para uma sequência de tamanho 5
+//0 1 1 2 3
+
+// Exemplo de saída para uma sequência de tamanho 10
+//0 1 1 2 3 5 8 13 21 34
+
+// Exemplo de saída para uma sequência de tamanho 10
+//0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181
---
+++ src/QualNumero.java
@@ -0,0 +1,23 @@
+public class QualNumero {
+    public static void main(String[] args) throws Exception {
+        int numero = -2;
+        String positivoNegativo = "";
+        
+        if(numero > 0){
+            positivoNegativo = "positivo";
+        }else{
+            positivoNegativo = "negativo";
+        }
+       
+        if(numero % 2 == 0){
+           System.out.println("É par e " + positivoNegativo);
+        } else {
+            System.out.println("É impar e " + positivoNegativo);
+        }
+    }
+}
+
+// Faça um algoritmo para receber um número qualquer e imprimir na tela se o número é par ou ímpar, positivo ou negativo.
+// Exemplos de Saída
+// -5 é ímpar e negativo.
+// 10 é par e positivo.
